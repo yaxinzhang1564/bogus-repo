cf40bc7db0d90500ac6b6f1c9f24757f9a5aabe8
Yaxin Zhang <yzhang@meditech.cloud>
2023-02-27 13:31:59 -0500

    add variable to subfolders

---
+++ VVV/local_file_exp/variables.tf
@@ -0,0 +1,7 @@
+variable "tenant_info" {
+  description = "tenant related info"
+}
+
+variable "wave_deploy_through" {
+  description = "The waves whose numbers are greater than this will NOT be deployed"
+}
